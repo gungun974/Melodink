4a262acdb669084b92dccf537501bfd528d751d7
Gungun974 <xfelix974@gmail.com>
2024-01-31 23:56:52 +0400

    Add flutter base app

---
+++ client/android/app/src/main/kotlin/com/example/melodink_client/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.melodink_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- flake.nix
+++ flake.nix
@@ -22,7 +22,18 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
+        config = {
+          android_sdk.accept_license = true;
+          allowUnfree = true;
+        };
       };
+      buildToolsVersion = "34.0.0";
+      androidComposition = pkgs.androidenv.composeAndroidPackages {
+        buildToolsVersions = [buildToolsVersion "28.0.3"];
+        platformVersions = ["34" "28"];
+        abiVersions = ["armeabi-v7a" "arm64-v8a"];
+      };
+      androidSdk = androidComposition.androidsdk;
     in {
       packages = {
         melodink-server = pkgs.buildGo121Module {
@@ -43,14 +54,27 @@
             make prebuild
           '';
         };
+
+        melodink-client = pkgs.flutter.buildFlutterApplication {
+          pname = "melodink-client";
+          version = "1.0.0";
+
+          src = gitignore.lib.gitignoreSource ./client;
+
+          autoPubspecLock = ./client/pubspec.lock;
+        };
       };
 
       devShell = pkgs.mkShell {
+        ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
         GOROOT = "${pkgs.go_1_21}/share/go";
         buildInputs = with pkgs; [
           (golangci-lint.override {buildGoModule = buildGo121Module;})
           go_1_21
           air
+          flutter
+          androidSdk
+          jdk17
         ];
       };
     });
