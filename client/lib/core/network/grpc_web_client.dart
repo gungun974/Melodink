5c8558376b6682102fa47921e7eaee79fd22d9b2
Gungun974 <xfelix974@gmail.com>
2024-04-24 02:08:50 +0400

    Cleanup old grpc files

--- client/lib/core/network/grpc_web_client.dart
+++
@@ -1,7 +0,0 @@
-import 'package:grpc/grpc_connection_interface.dart';
-import 'package:grpc/grpc_web.dart';
-import 'package:melodink_client/config.dart';
-
-ClientChannelBase createGrpcClient() {
-  return GrpcWebClientChannel.xhr(Uri.parse(appUrl));
-}
