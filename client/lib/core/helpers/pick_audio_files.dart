8817c06b5a08cff33924a789f5ce580207765dac
Gungun974 <xfelix974@gmail.com>
2024-12-09 21:28:33 +0400

    Specify in pickAudioFile allowedExtensions

--- client/lib/core/helpers/pick_audio_files.dart
+++ client/lib/core/helpers/pick_audio_files.dart
@@ -5,6 +5,16 @@ import 'package:file_picker/file_picker.dart';
 Future<File?> pickAudioFile() async {
   FilePickerResult? result = await FilePicker.platform.pickFiles(
     type: FileType.audio,
+    allowedExtensions: [
+      "mp3",
+      "mp4",
+      "m4a",
+      "ogg",
+      "oga",
+      "aac",
+      "wav",
+      "flac",
+    ],
   );
 
   if (result == null) {
@@ -18,6 +28,16 @@ Future<List<File>> pickAudioFiles() async {
   FilePickerResult? result = await FilePicker.platform.pickFiles(
     allowMultiple: true,
     type: FileType.audio,
+    allowedExtensions: [
+      "mp3",
+      "mp4",
+      "m4a",
+      "ogg",
+      "oga",
+      "aac",
+      "wav",
+      "flac",
+    ],
   );
 
   if (result == null) {
