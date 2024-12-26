e22a9b9d79152db4e96fa85d4802ecf79948c512
Gungun974 <xfelix974@gmail.com>
2024-12-27 01:06:15 +0400

    Add missing `Material` widget on AppConfirmModal

--- client/lib/core/helpers/app_confirm.dart
+++ client/lib/core/helpers/app_confirm.dart
@@ -75,73 +75,77 @@ class _AppConfirmModal extends StatelessWidget {
         horizontal: 16,
         vertical: 64,
       ),
-      child: Center(
-        child: IntrinsicWidth(
-          child: IntrinsicHeight(
-            child: Stack(
-              children: [
-                GradientBackground(),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 16.0,
-                    right: 16.0,
-                    top: 24.0,
-                    bottom: 12.0,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 8.0, right: 16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+      child: Material(
+        color: Colors.transparent,
+        child: Center(
+          child: IntrinsicWidth(
+            child: IntrinsicHeight(
+              child: Stack(
+                children: [
+                  GradientBackground(),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 16.0,
+                      right: 16.0,
+                      top: 24.0,
+                      bottom: 12.0,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding:
+                              const EdgeInsets.only(left: 8.0, right: 16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              if (title != null)
+                                Padding(
+                                  padding: const EdgeInsets.only(bottom: 8.0),
+                                  child: Text(
+                                    title!,
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 18,
+                                      letterSpacing: 18 * 0.04,
+                                    ),
+                                  ),
+                                ),
+                              if (content != null)
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      top: 8.0, bottom: 16.0),
+                                  child: Text(
+                                    content!,
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                      letterSpacing: 14 * 0.04,
+                                    ),
+                                  ),
+                                ),
+                            ],
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.end,
                           children: [
-                            if (title != null)
-                              Padding(
-                                padding: const EdgeInsets.only(bottom: 8.0),
-                                child: Text(
-                                  title!,
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 18,
-                                    letterSpacing: 18 * 0.04,
-                                  ),
-                                ),
-                              ),
-                            if (content != null)
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                    top: 8.0, bottom: 16.0),
-                                child: Text(
-                                  content!,
-                                  style: const TextStyle(
-                                    fontSize: 14,
-                                    letterSpacing: 14 * 0.04,
-                                  ),
-                                ),
-                              ),
+                            TextButton(
+                              onPressed: () => Navigator.pop(context, false),
+                              child: textCancel,
+                            ),
+                            SizedBox(width: 4),
+                            TextButton(
+                              onPressed: () => Navigator.pop(context, true),
+                              child: textOK,
+                            ),
                           ],
                         ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          TextButton(
-                            onPressed: () => Navigator.pop(context, false),
-                            child: textCancel,
-                          ),
-                          SizedBox(width: 4),
-                          TextButton(
-                            onPressed: () => Navigator.pop(context, true),
-                            child: textOK,
-                          ),
-                        ],
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
