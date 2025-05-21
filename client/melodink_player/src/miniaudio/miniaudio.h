5a8e35ba1bdd5c936618c81203274e77c1615c1c
Gungun974 <xfelix974@gmail.com>
2025-05-21 14:19:36 +0400

    a

--- client/melodink_player/src/miniaudio/miniaudio.h
+++ client/melodink_player/src/miniaudio/miniaudio.h
@@ -3089,7 +3089,7 @@ The interruption notifications are used on mobile platforms for detecting when a
 due to things like an incoming phone call. Currently this is only implemented on iOS. None of the
 Android backends will report this notification.
 */
-typedef void (* ma_device_notification_proc)(const void* pNotification);
+typedef void (* ma_device_notification_proc)(const ma_device_notification* pNotification);
 
 
 /*
@@ -3132,7 +3132,7 @@ callback. The following APIs cannot be called from inside the callback:
 
 The proper way to stop the device is to call `ma_device_stop()` from a different thread, normally the main application thread.
 */
-typedef void (* ma_device_data_proc)(void* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount);
+typedef void (* ma_device_data_proc)(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount);
 
 
 
@@ -3156,7 +3156,7 @@ Remarks
 -------
 Do not restart or uninitialize the device from the callback.
 */
-typedef void (* ma_stop_proc)(void* pDevice);  /* DEPRECATED. Use ma_device_notification_proc instead. */
+typedef void (* ma_stop_proc)(ma_device* pDevice);  /* DEPRECATED. Use ma_device_notification_proc instead. */
 
 typedef enum
 {
